3723d540093a7e8c3d1b3e8c4cb95a0af7a8ea34
adarshakb <adarshakb@gmail.com>
2020-12-28 21:35:31 -0800

    added signals to RSI

--- src/analysis/ta/rsi.py
+++ src/analysis/ta/rsi.py
@@ -1,21 +1,44 @@
 import pandas
 import talib
+import numpy
 
 
-def getRSI(ticker, column='Close', timeperiod=14):
-    df = pandas.read_csv("../../../resources/tickerList/" + ticker + "/history.csv")
-    return talib.RSI(df[column].values, timeperiod)
+class RSI():
+    def __init__(self, ticker, column='Close', timeperiod=14):
+        self.ticker = ticker
+        self.column = column
+        self.timeperiod = timeperiod
+        self.df = pandas.read_csv("../../../resources/tickerList/" + ticker + "/history.csv", float_precision="round_trip")
+
+    def getRSI(self):
+        """
+
+        :return: numpy.ndarray
+        """
+        return talib.RSI(self.df[self.column].values, self.timeperiod)
+
+    def getRSIForAll(self):
+        result = []
+        companiesList = pandas.read_csv("../../../resources/tickerList/NASDAQ-100-Stock-Tickers-List.csv")
+        for index, row in companiesList.iterrows():
+            company = row['Ticker']
+            result.append({
+                company: RSI(company, self.column, self.timeperiod).getRSI()
+            })
+        return result
+
+    def getSignals(self, low=30, high=70):
+        rsiSignals = self.getRSI()
+        indexs = numpy.argwhere(numpy.logical_or(rsiSignals < low, rsiSignals > high))
+        for index in indexs:
+            yield {
+                'index' : index[0],
+                'rsiValue' : rsiSignals[index[0]],
+                'history' : self.df.iloc[index[0]].values.tolist()
+            }
 
 
-def getRSIForAll(column='Close', timeperiod=14):
-    result = []
-    df = pandas.read_csv("../../../resources/tickerList/NASDAQ-100-Stock-Tickers-List.csv")
-    for index, row in df.iterrows():
-        company = row['Ticker']
-        result.append({
-            company: getRSI(company, column, timeperiod)
-        })
-    return result
+# print(RSI(ticker='aapl').getSignals())
 
-
-# print(getRSIForAll())
+# for signal in RSI(ticker='aapl').getSignals():
+#     print(signal)
