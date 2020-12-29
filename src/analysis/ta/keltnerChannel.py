0403c527518e9c2e63d9b1967aac8c99067a5621
adarshakb <adarshakb@gmail.com>
2020-12-28 22:12:19 -0800

    add signals to keltner channel

--- src/analysis/ta/keltnerChannel.py
+++ src/analysis/ta/keltnerChannel.py
@@ -1,29 +1,76 @@
 import pandas
 import talib
+import numpy
+
+class KeltnerChannel():
+    def __init__(self, ticker, column='Close', timeperiod=20, atrMultiple=2):
+        """
+        Keltner channel https://www.investopedia.com/terms/k/keltnerchannel.asp
+        :param ticker: the market ticker
+        :param column: Column in the data frame csv
+        :param timeperiod: input for EMA and ATR
+        """
+        self.ticker = ticker
+        self.column = column
+        self.timeperiod = timeperiod
+        self.atrMultiple = atrMultiple
+        self.df = pandas.read_csv("../../../resources/tickerList/" + ticker + "/history.csv")
+
+    def getKeltnerChannel(self):
+        """
+        :return: lower band, middle band, upper band
+        """
+        ema = talib.EMA(self.df[self.column].values, self.timeperiod)
+        atr = talib.ATR(self.df['High'].values, self.df['Low'].values, self.df['Close'].values, self.timeperiod)
+        return ema - (self.atrMultiple * atr), ema, ema + (self.atrMultiple * atr)
+
+    def getKeltnerChannelForAll(self):
+        result = []
+        df = pandas.read_csv("../../../resources/tickerList/NASDAQ-100-Stock-Tickers-List.csv")
+        for index, row in df.iterrows():
+            company = row['Ticker']
+            result.append({
+                company: KeltnerChannel(company).getKeltnerChannel()
+            })
+        return result
+    def getSignals(self):
+        """
+
+        generate signals when stock price is above upper band or below lower bands
+        :return:
+        """
+
+        # below are 1d numpy.ndarray
+        upperBand, middleBand, lowerBand = self.getKeltnerChannel()
+        columnPrices = self.df[self.column].values
+
+        for idx, price in numpy.ndenumerate(columnPrices):
+            if idx[0] >= self.timeperiod - 1:
+                # print(idx[0], price, upperBand[idx], middleBand[idx], lowerBand[idx])
+                if price > upperBand[idx]:
+                    yield {
+                        'index' : idx[0],
+                        'type': 'CROSS_UPPER_BAND',
+                        'price': price,
+                        'upperBand': upperBand[idx],
+                        'middleBand': middleBand[idx],
+                        'lowerBand': lowerBand[idx],
+                        'history': self.df.iloc[idx[0]].values.tolist()
+                    }
+                if price < lowerBand[idx]:
+                    yield {
+                        'index' : idx[0],
+                        'type': 'CROSS_LOWER_BAND',
+                        'price': price,
+                        'upperBand': upperBand[idx],
+                        'middleBand': middleBand[idx],
+                        'lowerBand': lowerBand[idx],
+                        'history': self.df.iloc[idx[0]].values.tolist()
+                    }
 
 
-def getKeltnerChannel(ticker, column='Close', timeperiod=20, atrMultiple=2):
-    """
-    Keltner channel https://www.investopedia.com/terms/k/keltnerchannel.asp
-    :param ticker: the market ticker
-    :param column: Column in the data frame csv
-    :param timeperiod: input for EMA and ATR
-    :return: lower band, middle band, upper band
-    """
-    df = pandas.read_csv("../../../resources/tickerList/" + ticker + "/history.csv")
-    ema = talib.EMA(df[column].values, timeperiod)
-    atr = talib.ATR(df['High'].values, df['Low'].values, df['Close'].values, timeperiod)
-    return ema - (atrMultiple * atr), ema, ema + (atrMultiple * atr)
 
+# print(KeltnerChannel('aapl').getKeltnerChannel())
 
-def getKeltnerChannelForAll(column='Close', timeperiod=20, atrMultiple=2):
-    result = []
-    df = pandas.read_csv("../../../resources/tickerList/NASDAQ-100-Stock-Tickers-List.csv")
-    for index, row in df.iterrows():
-        company = row['Ticker']
-        result.append({
-            company: getKeltnerChannel(company, column, timeperiod, atrMultiple)
-        })
-    return result
-
-# print(getKeltnerChannel('aapl'))
+for signal in KeltnerChannel('aapl').getSignals():
+    print(signal)
